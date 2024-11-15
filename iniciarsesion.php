925de0d77fa253b460b5a7aa5878025fb615c59e
ARMANDO DE JESÚS PÉREZ BOLÍVAR <armandod-perezb@unilibre.edu.co>
2024-11-14 22:56:01 -0500

    Creación del código inicio de sesión
    conexión a base de datos

---
+++ iniciarsesion.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "gestiondecitas";
+    $conexion = new mysqli($server, $user, $pass, $db);
+    if ($conexion->connect_errno) {
+    die("Conexion Fallida" .
+    $conexion->connect_errno);
+    } 
+    // Inicializar la variable de error
+    $error = "";
+    
+    $id = $_POST["id"];
+    $nombres = $_POST["nombres"];
+    $apellidos = $_POST["apellidos"];
+    $contrasena = $_POST["contrasena"];
+    $telefono = $_POST["telefono"];
+    $email = $_POST["email"];
+    $direccion = $_POST["direccion"];
+
+    $consulta = "SELECT * FROM usuario WHERE id = '$id' AND contrasena = '$contrasena'";;
+    try{
+        $resultado = mysqli_query($conexion, $consulta);
+        try{
+            if($resultado){
+                if($resultado->num_rows > 0){
+                    $user = $resulto->fetch_assoc();
+                    // Agrega esta línea para ver todos los datos recuperados
+                    var_dump($user); // Eliminar esta línea una vez confirmado el contenido
+
+                    $_SESSION['user_id'] = $user['id'];
+                    $_SESSION['user_email'] = $user['email'];
+                    $_SESSION['user_nombres'] = $user['nombres'];
+                    $_SESSION['user_apellidos'] = $user['apellidos'];
+                    $_SESSION['user_direccion'] = $user['direccion'];
+                    header("Location: Calendario.html");
+                    exit();
+                }else{
+                    echo "Id o Contraseña incorrectos";
+                }
+                
+        }else{
+            ?> <h1>Inicio de sesión fallido</h1> <?php
+        }
+        
+        }catch(mysqli_sql_exception $e){
+            $error = "Error en el registro";
+            echo $error;
+        }
+    }catch(mysqli_sql_exception $e) {
+        // Manejar error de entrada duplicada
+        if ($e->getCode() === 1062) {
+            $error = "Error: El id ya está registrado. Por favor, use uno diferente.";
+            echo $error;
+        } else {
+            // Manejar otros errores de MySQLi
+            $error = "Error al registrar el usuario: " . $e->getMessage();
+            echo $error;
+        }
+    }
+        ?>
